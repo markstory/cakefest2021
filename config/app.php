facd75fdc843eda4ad7c2cf909c2339c3a1aa029
Mark Story <mark@mark-story.com>
2021-10-03 23:09:02 -0400

    Update deps and update config to work around bug.

--- config/app.php
+++ config/app.php
@@ -366,14 +366,6 @@ return [
             'scopes' => false,
             'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
         ],
-        // To enable this dedicated query log, you need set your datasource's log flag to true
-        'queries' => [
-            'className' => FileLog::class,
-            'path' => LOGS,
-            'file' => 'queries',
-            'url' => env('LOG_QUERIES_URL', null),
-            'scopes' => ['queriesLog'],
-        ],
     ],
 
     /*
